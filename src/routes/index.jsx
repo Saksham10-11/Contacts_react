e108a99161aea709180ee7e0b9c8cd1466a8068a
Saksham <Sakshamsaipatwarpkd@gmail.com>
2024-07-20 13:01:27 +0530

    add modification options

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is simple Contacts App made with React using react-router
+      <br />
+      Check out{" "}
+      <a href="https://github.com/Saksham10-11/Contacts_react">
+        the source code.
+      </a>
+      .
+    </p>
+  );
+}
